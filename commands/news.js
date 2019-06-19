a6ce8d9030251845acc9734c21fd7f4f865626ff
anacle33 <48806914+anacle33@users.noreply.github.com>
2019-06-19 23:37:35 +0200

    Create news.js

---
+++ commands/news.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+    message.delete();
+    if(args[0] == "help"){
+      message.reply("Usage: !news <votre news>");
+      return;
+    }
+    let rUser = message.author;
+    let raison = args.join(" ");
+    
+    message.reply("votre annonce a bien été envoyé")
+
+    let reportschannel = message.guild.channels.find(`name`, "「🌍」annonce");
+    if(!reportschannel) return message.channel.send("Je ne trouve pas le channel demande-de-rdv-admin");
+    reportschannel.send(raison);
+
+}
+
+module.exports.help = {
+  name: "news"
+}
