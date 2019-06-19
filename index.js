d4a40a88eeeebbfa2cf9d2318f2d02ae774978d6
anacle33 <48806914+anacle33@users.noreply.github.com>
2019-06-19 21:36:47 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+const bot = new Discord.Client();
+
+var prefix = "!";
+
+bot.on(message, () => {
+let args = message.content.slice(prefix.length).trim().split(" ");
+    let cmds = args.shift().toLowerCase();
+
+// Return Statements
+    if (sender.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+	try {
+        let commandFile = require(`./commands/${cmds}.js`);
+        commandFile.run(bot, message, args, prefix);
+    } catch(e) {
+        console.log(e.message);
+    } finally {
+        console.log(`${message.author.username} ran the command: ${cmds}`);
+    }
+});
+
+bot.login(process.env.token);
