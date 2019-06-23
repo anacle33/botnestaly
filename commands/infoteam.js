6a3941a4a6a26e92930f509e6c8a30b0c4ad579d
anacle33 <48806914+anacle33@users.noreply.github.com>
2019-06-23 14:34:17 +0200

    Update infoteam.js

--- commands/infoteam.js
+++ commands/infoteam.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
 Nous sommes une team fortnite crée depuis janvier 2019. Cette team est faite pour s'améliorer au maximum ! 
 Pour entrer dans la team, aller faire une demande de rendez-vous dans le salon <#574873322890723338> , 
 puis vous aurez une convocation avec les personnes du staff pour vous testez ! 
-Si vous avez des questions par rapport à cela, demander aux Administrateurs ou au Fondateur  !
+Si vous avez des questions par rapport à cela, demander aux Administrateurs ou le Fondateur  !
 
 De plus, n'hésitez pas à publier le serveur en masse pour jouer tous ensemble que se soit ami ou avec la team !
 Voici le lien : https://discord.gg/epSgNPj
